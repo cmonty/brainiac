c32bfd1ada84b022b8e928a5f7a33de736c32a0b
Michael Pilat <mike@mikepilat.com>
2012-01-29 15:25:00 -0600

    Add plugin for Jenkins build status

---
+++ src/brainiac/plugins/jenkins_builds.clj
@@ -0,0 +1,38 @@
+(ns brainiac.plugins.jenkins-builds
+  (:require [brainiac.plugin :as brainiac]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]
+            [clojure.contrib.zip-filter.xml :as zf]))
+
+(defn parse-project [node]
+  (let [project-name (zf/xml1-> node (zf/attr :name))
+        status (zf/xml1-> node (zf/attr :lastBuildStatus))]
+    (assoc {}
+      :name project-name
+      :status status)))
+
+(defn filter-status [builds status]
+  (filter #(= (:status %) status) builds))
+
+(defn build-status-string [build-data]
+  (let [fail-count (count (filter-status build-data "Failure"))]
+    (cond
+      (> fail-count 0) (format "%d Failing Builds" fail-count)
+      :default "All builds passing")))
+
+(defn transform [stream]
+  (let [xml (zip/xml-zip (xml/parse stream))
+        build-data (zf/xml-> xml :Project parse-project)]
+    (assoc {}
+      :name "jenkins-builds"
+      :type "list"
+      :title (str "Jenkins: " (build-status-string build-data))
+      :data (map #(:name %) (filter-status build-data "Failure")))))
+
+(defn configure [{:keys [url username password]}]
+  (brainiac/schedule
+    60000
+    (brainiac/simple-http-plugin
+      {:method :get :url url :basic-auth [username password]}
+      transform)))
+
---
+++ test/brainiac/test/plugins/jenkins_builds.clj
@@ -0,0 +1,29 @@
+(ns brainiac.test.plugins.jenkins-builds
+  (:use [brainiac.plugins.jenkins-builds]
+        [clojure.test]))
+
+(def example-xml-with-failures (java.io.ByteArrayInputStream. (.getBytes "<Projects><Project name=\"project1\" lastBuildStatus=\"Success\"/><Project name=\"project2\" lastBuildStatus=\"Failure\"/><Project name=\"project3\" lastBuildStatus=\"Failure\"/><Project name=\"project4\" lastBuildStatus=\"Unknown\"/></Projects>")))
+
+(def example-xml-no-failures (java.io.ByteArrayInputStream. (.getBytes "<Projects><Project name=\"project1\" lastBuildStatus=\"Success\"/><Project name=\"project2\" lastBuildStatus=\"Success\"/><Project name=\"project3\" lastBuildStatus=\"Success\"/></Projects>")))
+
+(deftest test-transform-with-failures
+  (let [result (transform example-xml-with-failures)]
+    (testing "sets name"
+      (is (= "jenkins-builds" (:name result))))
+    (testing "sets type"
+      (is (= "list" (:type result))))
+    (testing "sets title"
+      (is (= "Jenkins: 2 Failing Builds" (:title result))))
+    (testing "sets data"
+      (is (= ["project3" "project5"])))))
+
+(deftest test-transform-without-failures
+  (let [result (transform example-xml-no-failures)]
+    (testing "sets name"
+      (is (= "jenkins-builds" (:name result))))
+    (testing "sets type"
+      (is (= "list" (:type result))))
+    (testing "sets title"
+      (is (= "Jenkins: All builds passing" (:title result))))
+    (testing "sets data"
+      (is (= [])))))
