17ad8bb52c26c2622f1ad06634994653a2893a27
Cory Monty <cory.monty@gmail.com>
2012-05-15 21:40:24 -0500

    Upgrades Aleph for Google Chrome 19+ websocket compression

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [aleph "0.2.0"]
+                 [aleph "0.3.0-alpha2"]
                  [clj-yaml "0.3.1"]
                  [commons-lang "2.6"]
                  [net.sourceforge.htmlcleaner/htmlcleaner "2.2"]
--- src/brainiac/websocket.clj
+++ src/brainiac/websocket.clj
@@ -3,21 +3,19 @@
   (:require [lamina.core :as lamina]))
 
 (def recent-updates (atom {}))
+(def broadcast-channel (lamina/channel))
 
 (defn store-recent-update! [json-message]
   (let [message (read-json json-message)]
     (swap! recent-updates assoc (:name message) json-message))
   json-message)
 
-(defn websocket-for-program [program-name]
-  (lamina/named-channel program-name))
-
 (defn broadcast-json [json-message program-name]
   (store-recent-update! json-message)
-  (lamina/enqueue (websocket-for-program program-name) json-message))
+  (lamina/enqueue broadcast-channel json-message))
 
-(defn subscribe-to-updates [channel]
-  (lamina/receive channel
+(defn subscribe-to-updates [ch]
+  (lamina/receive ch
     (fn [program]
-      (lamina/siphon (websocket-for-program program) channel)
+      (lamina/siphon broadcast-channel ch)
       (doseq [m (vals @recent-updates)] (broadcast-json m program)))))
