6c22847e174aa0d8f4285a29b77cd88329a302ef
Cory Monty <cory.monty@gmail.com>
2012-05-18 12:46:36 -0500

    Changes project version to standardize for deployments.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brainiac "1.0.0-SNAPSHOT"
+(defproject brainiac "core"
   :description "An Awesome HUD"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
