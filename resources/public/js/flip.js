403852ba98aa914f01e458e188d1703f6dfe9338
Michael Pilat <mike@mikepilat.com>
2012-05-29 11:55:23 -0500

    Use window.setInterval instead of recursive setTimeout calls for flipping.

--- resources/public/js/flip.js
+++ resources/public/js/flip.js
@@ -1,21 +1,11 @@
-function flipPanel($elem) {
-  $elem.toggleClass('flip');
-  setTimeout(function() {
-    flipPanel($elem);
-  }, 12000);
-}
-
-function scheduleFlip($elem, initialDelay) {
-  $elem.delay(initialDelay).queue(function() {
-    flipPanel($elem);
-  });
-}
-
 $(document).ready(function() {
   $('.panel').each(function(index, elem) {
-    $elem = $(elem);
-    if($elem.children('div').length > 1) {
-      scheduleFlip($(elem), index * 2000);
+    if($(elem).children('div').length > 1) {
+      $(elem).delay(index * 2000).queue(function () {
+        window.setInterval(function() {
+          $(elem).toggleClass('flip');
+        }, 12000);
+      });
     }
   });
 });
