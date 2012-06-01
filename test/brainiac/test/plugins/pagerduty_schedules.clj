854904becd3384faf6adc69cb620a4e576e6e8fa
Paul Gross <pgross@gmail.com>
2012-06-01 16:26:16 -0500

    Fix pagerduty_schedules test.

--- test/brainiac/test/plugins/pagerduty_schedules.clj
+++ test/brainiac/test/plugins/pagerduty_schedules.clj
@@ -3,13 +3,14 @@
         [clojure.test]
         [clojure.contrib.mock]))
 
-(def schedules-json (java.io.ByteArrayInputStream. (.getBytes (slurp "test/brainiac/test/data/pagerduty_schedules.json"))))
+(def schedules-json1 (java.io.ByteArrayInputStream. (.getBytes (slurp "test/brainiac/test/data/pagerduty_schedules.json"))))
+(def schedules-json2 (java.io.ByteArrayInputStream. (.getBytes (slurp "test/brainiac/test/data/pagerduty_schedules.json"))))
 
 (deftest test-transform
-  (let [parsed-result (transform schedules-json)]
+  (let [parsed-result (transform [schedules-json1 schedules-json2])]
     (testing "sets name"
       (is (= "pagerduty-schedules" (:name parsed-result))))
     (testing "sets data"
-      (is (= "Ali Aghareza" (:name (:user (first (:data parsed-result)))))))
+      (is (= "Ali Aghareza" (:primary_name parsed-result))))
     (testing "sets type"
       (is (= "schedule" (:type parsed-result))))))
