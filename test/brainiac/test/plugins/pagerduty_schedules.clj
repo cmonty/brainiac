533ebab5f5fdcd3a32b416b46834bf31ea54f68d
Paul Gross <pgross@gmail.com>
2012-06-01 16:39:58 -0500

    Clean up pagerduty_schedules test and add assertions for backup on-call.

--- test/brainiac/test/plugins/pagerduty_schedules.clj
+++ test/brainiac/test/plugins/pagerduty_schedules.clj
@@ -3,14 +3,21 @@
         [clojure.test]
         [clojure.contrib.mock]))
 
-(def schedules-json1 (java.io.ByteArrayInputStream. (.getBytes (slurp "test/brainiac/test/data/pagerduty_schedules.json"))))
-(def schedules-json2 (java.io.ByteArrayInputStream. (.getBytes (slurp "test/brainiac/test/data/pagerduty_schedules.json"))))
+(defn schedules-json [name]
+  (str "{\"total\":1,\"entries\":[{\"end\":\"2012-01-23T08:55:00-06:00\",\"start\":\"2012-01-23T08:55:00-06:00\",\"user\":{\"name\":\"" name "\",\"id\":\"PAVOS91\",\"color\":\"purple\",\"email\":\"ali@getbraintree.com\"}}]}"))
+
+(def primary-json (java.io.ByteArrayInputStream. (.getBytes (schedules-json "Ali Aghareza"))))
+(def backup-json (java.io.ByteArrayInputStream. (.getBytes (schedules-json "Paul Gross"))))
 
 (deftest test-transform
-  (let [parsed-result (transform [schedules-json1 schedules-json2])]
+  (let [parsed-result (transform [primary-json backup-json])]
     (testing "sets name"
       (is (= "pagerduty-schedules" (:name parsed-result))))
-    (testing "sets data"
-      (is (= "Ali Aghareza" (:primary_name parsed-result))))
+    (testing "primary"
+      (is (= "Ali Aghareza" (:primary_name parsed-result)))
+      (is (= "Ali_Aghareza" (:primary_name_image parsed-result))))
+    (testing "backup"
+      (is (= "Paul Gross" (:backup_name parsed-result)))
+      (is (= "Paul_Gross" (:backup_name_image parsed-result))))
     (testing "sets type"
       (is (= "schedule" (:type parsed-result))))))
