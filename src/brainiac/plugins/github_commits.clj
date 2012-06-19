4f926356e2e67657906c7f28500bdd04f8b48eec
Michael Boeke <mvboeke@gmail.com>
2012-06-18 23:29:32 -0500

    updated styles for Github panel

--- src/brainiac/plugins/github_commits.clj
+++ src/brainiac/plugins/github_commits.clj
@@ -5,20 +5,25 @@
             [brainiac.pages.templates :as templates]))
 
 (defn format-commit [commit]
-  (format "%s - %s - %s" (.substring (:sha commit) 0 10) (:login (:author commit)) (:message (:commit commit))))
+  (format "%s - %s" (:login (:author commit)) (:message (:commit commit))))
 
 (defn transform [username repository stream]
   (let [json (read-json (reader stream))]
   (assoc {}
     :name "github-commits"
-    :type "list"
+    :type "github-commits"
     :title (format "Recent commits to %s/%s" username repository)
     :data (map format-commit (take 5 json)))))
 
 (defn url [username repository]
   (format "https://api.github.com/repos/%s/%s/commits" username repository))
 
-(defn html [] (templates/unordered-list))
+(defn html [] 
+  [:script#github-commits-template {:type "text/mustache"}
+    "<div id='octobox'>
+      <h2>{{title}}</h2>
+      <ul> {{#data}}<li>{{.}}</li>{{/data}} </ul>
+      </div>"])
 
 (defn configure [{:keys [username repository program-name]}]
   (brainiac/simple-http-plugin
