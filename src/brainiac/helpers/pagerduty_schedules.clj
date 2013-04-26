1215495ad00534aaad4eef33dc6576cc70b4cf02
Michael Pilat <mike@mikepilat.com>
2013-04-26 14:28:42 -0500

    Use Gravatar images for PagerDuty schedule plugin
    No longer hard-codes paths to the Braintree team page

--- src/brainiac/helpers/pagerduty_schedules.clj
+++ src/brainiac/helpers/pagerduty_schedules.clj
@@ -4,7 +4,8 @@
   (:require [brainiac.plugin :as brainiac]
 						[clojure.string :as string]
             [clojure.contrib.json :as json]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:use [brainiac.helpers.gravatar :only [email->gravatar]]))
 
 (defn now []
   (.getTime (Calendar/getInstance)))
@@ -22,6 +23,9 @@
 (defn- json->name [json]
   (-> json :entries first :user :name))
 
+(defn- json->email [json]
+  (-> json :entries first :user :email))
+
 (defn- name->image [name]
   (string/replace name #" " "_"))
 
@@ -29,11 +33,15 @@
   (let [json-responses (map (comp json/read-json io/reader) streams)
         [primary-json backup-json] json-responses
         primary-name (json->name primary-json)
-        backup-name (json->name backup-json)]
+        backup-name (json->name backup-json)
+        primary-email (json->email primary-json)
+        backup-email (json->email backup-json)]
     {:primary_name primary-name
-     :primary_name_image (name->image primary-name)
+     :primary_email primary-email
+     :primary_gravatar (email->gravatar primary-email)
      :backup_name backup-name
-     :backup_name_image (name->image backup-name)}))
+     :backup_email backup-email
+     :backup_gravatar (email->gravatar backup-email)}))
 
 (defn- request [username password url]
   {:method :get
--- test/brainiac/test/helpers/pagerduty_schedules.clj
+++ test/brainiac/test/helpers/pagerduty_schedules.clj
@@ -3,17 +3,19 @@
         [clojure.test]
         [clojure.contrib.mock]))
 
-(defn schedules-json [name]
-  (str "{\"total\":1,\"entries\":[{\"end\":\"2012-01-23T08:55:00-06:00\",\"start\":\"2012-01-23T08:55:00-06:00\",\"user\":{\"name\":\"" name "\",\"id\":\"PAVOS91\",\"color\":\"purple\",\"email\":\"ali@getbraintree.com\"}}]}"))
+(defn schedules-json [name email]
+  (str "{\"total\":1,\"entries\":[{\"end\":\"2012-01-23T08:55:00-06:00\",\"start\":\"2012-01-23T08:55:00-06:00\",\"user\":{\"name\":\"" name "\",\"id\":\"PAVOS91\",\"color\":\"purple\",\"email\":\"" email "\"}}]}"))
 
-(def primary-json (java.io.ByteArrayInputStream. (.getBytes (schedules-json "Ali Aghareza"))))
-(def backup-json (java.io.ByteArrayInputStream. (.getBytes (schedules-json "Paul Gross"))))
+(def primary-json (java.io.ByteArrayInputStream. (.getBytes (schedules-json "Ali Aghareza" "ali@getbraintree.com"))))
+(def backup-json (java.io.ByteArrayInputStream. (.getBytes (schedules-json "Paul Gross" "pgross@getbraintree.com"))))
 
 (deftest test-transform
   (let [parsed-result (transform [primary-json backup-json])]
     (testing "primary"
       (is (= "Ali Aghareza" (:primary_name parsed-result)))
-      (is (= "Ali_Aghareza" (:primary_name_image parsed-result))))
+      (is (= "ali@getbraintree.com", (:primary_email parsed-result)))
+      (is (= "https://secure.gravatar.com/avatar/cb47210be3a8be90e2af65156e57b324", (:primary_gravatar parsed-result)))
     (testing "backup"
       (is (= "Paul Gross" (:backup_name parsed-result)))
-      (is (= "Paul_Gross" (:backup_name_image parsed-result))))))
+      (is (= "pgross@getbraintree.com", (:backup_email parsed-result)))
+      (is (= "https://secure.gravatar.com/avatar/6d804cfe97a2d8f9c669a1da65993c5c", (:backup_gravatar parsed-result)))))))
