f0b7d9ba2f0f9aa1008deacec787b6d933cfe799
Stephen Gelman <stephen.gelman@braintreepayments.com>
2013-10-11 17:40:49 -0500

    Fix typo from old hp-access-point plugin test

--- test/brainiac/test/plugins/hp_access_point.clj
+++ test/brainiac/test/plugins/hp_access_point.clj
@@ -4,7 +4,7 @@
 
 (def example-xml (java.io.ByteArrayInputStream. (.getBytes "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:PROCURVE-MOBILITY-MSM=\"http://www.procurve_mobility_msm.com/SOAP/API/1.7/\" xmlns:VERSION=\"http://www.procurve_mobility_msm.com/SOAP/API/1.0/\"><SOAP-ENV:Body><PROCURVE-MOBILITY-MSM:GetVirtualSCPSKResponse><PROCURVE-MOBILITY-MSM:psk>super secret</PROCURVE-MOBILITY-MSM:psk></PROCURVE-MOBILITY-MSM:GetVirtualSCPSKResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>")))
 
-(deftest test-transfor
+(deftest test-transform
   (let [result (transform "FBI Van" example-xml)]
     (testing "sets name"
       (is (= "hp-access-point" (:name result))))
