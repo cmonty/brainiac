b0a7dab93e1ec4ceaf0a2d6cbf876895cdac5de0
Stephen Gelman <stephen.gelman@braintreepayments.com>
2013-11-25 17:07:28 -0600

    Add test case for MD5 zero-padded hash

--- test/brainiac/test/helpers/md5.clj
+++ test/brainiac/test/helpers/md5.clj
@@ -7,5 +7,7 @@
     (is (= "" (md5/str->hash nil))))
   (testing "returns empty string if blank string"
     (is (= "" (md5/str->hash ""))))
+  (testing "pads short hashes with zero"
+    (is (= "0cc175b9c0f1b6a831c399e269772661" (md5/str->hash "a"))))
   (testing "hashes email using md5 algorithm"
     (is (= "55502f40dc8b7c769880b10874abc9d0" (md5/str->hash "test@example.com")))))
