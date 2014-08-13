902ca12c36cd0b858dcc4ff84314ca37c153c4a8
Stephen Gelman <ssgelm@gmail.com>
2014-08-13 10:52:12 -0500

    Fix tests
    Tweets use relative time and the test tweet is now 2 years old instead of 1...

--- test/brainiac/test/plugins/twitter_search.clj
+++ test/brainiac/test/plugins/twitter_search.clj
@@ -13,7 +13,7 @@
            {
               :name "hav u seen ?"
               :handle "Rutzen__"
-              :time "a year ago"
+              :time "2 years ago"
               :text "o Brainiac \u00e9 meio babac\u00e3o"
               :profile_image_url "https://si0.twimg.com/profile_images/378800000060004493/527b88a6db7d7d7e7975953a99666984_normal.png"
             }
