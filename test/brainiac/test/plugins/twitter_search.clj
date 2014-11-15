d1cbf4ba8d29ac9bea564340209ad5a6e35cf237
Stephen Gelman <ssgelm@gmail.com>
2014-11-14 22:35:50 -0600

    Remove :time from the twitter search test result
    Because twitter uses relative time unless :time is removed the test
    would need to be updated every year.

--- test/brainiac/test/plugins/twitter_search.clj
+++ test/brainiac/test/plugins/twitter_search.clj
@@ -9,11 +9,10 @@
     (testing "sets name"
       (is (= "twitter-search" (:name result))))
     (testing "sets data"
-      (is (= (first (:data result))
+      (is (= (dissoc (first (:data result)) :time)
            {
               :name "hav u seen ?"
               :handle "Rutzen__"
-              :time "2 years ago"
               :text "o Brainiac \u00e9 meio babac\u00e3o"
               :profile_image_url "https://si0.twimg.com/profile_images/378800000060004493/527b88a6db7d7d7e7975953a99666984_normal.png"
             }
