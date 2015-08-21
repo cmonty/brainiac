d2a463d3c3e3d001d09372b6d9a6fdbac54fe472
gary <gary@getbraintree.com>
2015-08-21 10:48:03 -0700

    Adds Muni tracker UI, example config, handles no-departure artifacts from API

--- src/brainiac/plugins/muni_tracker.clj
+++ src/brainiac/plugins/muni_tracker.clj
@@ -9,11 +9,25 @@
     :direction (zf/xml1-> route :RouteDirectionList :RouteDirection (zf/attr :Name))
     :departures (zf/xml-> route :RouteDirectionList :RouteDirection :StopList :Stop :DepartureTimeList :DepartureTime zf/text)))
 
+(defn has-departures? [route]
+  (seq (:departures route)))
+
 (defn transform [stream]
   (let [xml-zipper (xml/parse-xml stream)
-        routes (zf/xml-> xml-zipper :AgencyList :Agency :RouteList :Route route-with-departures)
+        routes (filter has-departures? (zf/xml-> xml-zipper :AgencyList :Agency :RouteList :Route route-with-departures))
         stop (zf/xml1-> xml-zipper :AgencyList :Agency :RouteList :Route :RouteDirectionList :RouteDirection :StopList :Stop (zf/attr :name))]
     (assoc {}
       :name "muni-tracker"
       :stop stop
       :routes routes)))
+
+(defn url [api-key stop-id]
+  (format "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=%s&stopcode=%s" api-key stop-id))
+
+(defn configure [{:keys [api-key stop-id program-name]}]
+  (brainiac/schedule
+    20000
+    (brainiac/simple-http-plugin
+      {:method "GET" :url (url api-key stop-id)}
+      transform program-name)))
+
